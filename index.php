eeda47d32879d995b7f03edfdc7a5a3cda9262a9
Sam's Humorous Online E-Web Site <150535297+SHOEWS@users.noreply.github.com>
2023-11-17 15:33:33 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html>
   <head>
     <title>SHOEWS</title>
-    <link rel="stylesheet" type="text/css" href="index.html">
+    <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
     <p>Sam's Humorous Online E-Web Site</p>
+    <?php echo "Hi!";?>
   </body>
 </html>
